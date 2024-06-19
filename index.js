fa48b001979667a31d2fa1136f136226738a112f
klexq <daniilevgaliev@yandex.ru>
2024-06-19 17:27:37 +0300

    complete

---
+++ index.js
@@ -0,0 +1,40 @@
+const {
+    Connection,
+    PublicKey,
+    clusterApiUrl,
+    Keypair,
+    LAMPORTS_PER_SOL
+} = require("@solana/web3.js")
+
+const wallet = new Keypair();
+
+const publicKey = new PublicKey(wallet._keypair.publicKey);
+const secretKey = wallet._keypair.secretKey;
+
+const getWalletBalance = async () => {
+    try {
+        const connection = new Connection(clusterApiUrl("devnet"), "confirmed")
+        const walletBalance = await connection.getBalance(publicKey)
+        console.log(`Wallet balance is ${walletBalance}`)
+    } catch(err) {
+        console.error(err)
+    }
+}
+
+const airdropSol = async () => {
+    try {
+        const connection = new Connection(clusterApiUrl("devnet"), "confirmed")
+        const airdrop = await connection.requestAirdrop(publicKey, 1 * LAMPORTS_PER_SOL)
+        await connection.confirmTransaction(airdrop)
+        console.log("Airdrop completed")
+    } catch(err) {
+        console.error(err)
+    }
+}
+
+const main = async () => {
+    await getWalletBalance()
+    await airdropSol()
+    await getWalletBalance()
+}
+main()
